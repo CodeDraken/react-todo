c11c4e5a7e1827a970a67fb34315fde39b50a0c3
CodeDraken <codedraken@gmail.com>
2017-01-22 19:45:09 -0800

    TodoAPI filter todos

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -16,5 +16,34 @@ export default {
     } catch (e) {}
     
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    let filteredTodos = todos;
+
+    // Filtered by showCompleted
+    filteredTodos = filteredTodos.filter( (todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // Filtered by searchText
+    filteredTodos = filteredTodos.filter ( (todo) => {
+      let todoText = todo.text.toLowerCase();
+      
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with incomplete first
+    filteredTodos.sort( (a,b) => {
+      // -1 a come first 1 b come first 0 equal
+      if ( !a.completed  && b.completed ) {
+        return -1;
+      } else if ( a.completed && !b.completed ) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/API/TodoAPI.test.jsx
+++ app/tests/API/TodoAPI.test.jsx
@@ -44,4 +44,36 @@ describe('TodoAPI', () => {
     });
   });
 
+  describe('filter todos', () => {
+    let todos = [
+      {id:1, text:'text', completed: true},
+      {id:2, text:'todo', completed: false},
+      {id:3, text:'coffee', completed: true}
+      ];
+    let filteredTodosT = TodoAPI.filterTodos(todos, true, '');
+    let filteredTodosF = TodoAPI.filterTodos(todos, false, '');
+
+    it('should return all todos if showCompleted is true', () => {
+      expect(filteredTodosT.length).toBe(3);
+    });
+
+    it('should return only incomplete todos if showCompleted is false', () => {
+      expect(filteredTodosF.length).toBe(1);
+    });
+
+    it('should sort incomplete before completed todos', () => {
+      expect(filteredTodosT[0].completed).toBe(false);
+    });
+
+    it('should filter todos by search text', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, 'coffee');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if search text empty', () => {
+      expect(filteredTodosT.length).toBe(3);
+    });
+
+  });
+
 });
